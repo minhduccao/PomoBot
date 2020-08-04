7c148a46ce24e874d84a045ea7df9b45a790244f
Minhduc Cao <minhduc.t.cao@gmail.com>
2020-08-03 17:45:02 -0700

    Completed simple timer functionality and commands

--- bot.py
+++ bot.py
@@ -2,11 +2,21 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+import configparser
+import asyncio
+
+from timer import Timer
+from timer import TimerStatus
 
 DEBUG = True                                    # For debug messages
+SETTING_OPTIONS = ['work_time', 'short_break_time', 'long_break_time', 'sessions', 'use_long_breaks']
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')              # Grabs Discord bot token from .env file
 bot = commands.Bot(command_prefix='*')
+timer = Timer()
+workTimeLeft = 0
+breakTimeLeft = 0
+
 
 # ------------ Overall Work List ---------
 # TODO: Complete remaining commands
@@ -14,7 +24,6 @@ bot = commands.Bot(command_prefix='*')
 # TODO: Store user-set times
 # TODO: Override *help command to be fancier (see Octave bot)
 # TODO: Add docstrings
-# TODO: Create logo for repo and bot icon
 # TODO: Create empty .env file before finalizing
 # TODO: Remove all DEBUG statements and check imports before finalizing
 
@@ -26,32 +35,78 @@ async def on_ready():
 
 @bot.command(name='start', help='Starts a Pomodoro timer for 25 minutes for work, 5 minutes for break')
 async def start_timer(ctx):
-    # TODO: Add default to timer and call from user-set timing
     # TODO: Adjust help message to be more general
-    # TODO: Start a bot timer with some trigger at the end of the timer
-    # TODO: Add a 'long break' functionality later
+    # TODO: Add break functionality
+    # TODO: Let *start also resume the timer if paused
+    # TODO: Add proper msgs for error handling + correct
     if DEBUG:
-        await ctx.send('Starting timer')
+        em = discord.Embed(title='Starting Timer', description='Starting timer', color=0x00FF00)
+        msg = await ctx.send('Starting timer')
+        await msg.edit(embed=em)
         print('Command: *start')
 
+    if timer.get_status() == TimerStatus.STOPPED:
+        work_time = int(config['CURRENT_SETTINGS']['work_time']) * 60
+        timer.start(work_time)
 
-@bot.command(name='settimer', help='Customizes the work/break amounts for the timer')
+        while timer.get_status() == TimerStatus.RUNNING:
+            await asyncio.sleep(1)
+            timer.tick()
+    else:
+        em = discord.Embed(title='Warning', description='Timer is already running.', color=0x00FF00)
+        msg = await ctx.send('Timer is already running')
+        await msg.edit(embed=em)
+        if DEBUG:
+            print('Timer already exists.')
+
+
+@bot.command(name='set', help='Customizes the work/break amounts for the timer')
 async def set_timer_amt(ctx, work_time: int, break_time: int):
     # TODO: Store user-specified times
     # TODO: Change to accept default values
     # TODO: Change input params to work with short and long break inputs
     if DEBUG:
         await ctx.send(f'Work: {work_time} Break: {break_time}')
-        print(f'Command: *settimer: Work Time: {work_time} Break Time: {break_time}')
+        print(f'Command: *set: Work Time: {work_time} Break Time: {break_time}')
+
+
+@bot.command(name='pause', help='Pauses the timer')
+async def pause_timer(ctx):
+    # TODO: Handle if timer is already paused or stopped already
+    if DEBUG:
+        await ctx.send('Paused timer')
+        print('Command: *pause')
+    if not timer.pause():
+        # Handle error here
+        pass
 
 
 @bot.command(name='stop', help='Stops the timer')
 async def stop_timer(ctx):
-    # TODO: Stop the current timer
     # TODO: Handle if timer is already stopped or hasn't started yet
     if DEBUG:
         await ctx.send('Stopped timer')
         print(f'Command: *stop')
+    if not timer.stop():
+        # Handle error here
+        pass
+
+
+@bot.command(name='reset', help='Reset timer settings to default values.')
+async def reset_settings(ctx):
+    for option in SETTING_OPTIONS:
+        config.set('CURRENT_SETTINGS', option, config['DEFAULT'][option])
+    with open('settings.ini', 'w') as configFile:
+        config.write(configFile)
+    await ctx.send('Finished resetting timer settings to default values.')
+
+
+# TODO: Remove command later
+@bot.command(name='t', help='Temporary for testing commands')
+async def t(ctx):
+    em = discord.Embed(title='Error', description='Desc', color=0x00FF00)
+    msg = await ctx.send()
+    await msg.edit(embed=em)
 
 
 # ----------------------- ERROR HANDLING -----------------------------
@@ -68,4 +123,8 @@ async def set_timer_amt_error(ctx, error):
         with open('error.log', 'a') as errorLog:
             errorLog.write(f'Unhandled *settimer message: {ctx.message.content} \n{ctx.message}\n')
 
-bot.run(TOKEN)
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read('settings.ini')                 # Read in settings from settings.ini
+    bot.run(TOKEN)
--- timer.py
+++ timer.py
@@ -10,9 +10,11 @@ class TimerStatus(Enum):
 class Timer:
     def __init__(self):
         self.status = TimerStatus.STOPPED
+        self.time_left = 0
 
-    def start(self):
+    def start(self, duration: int):
         if self.status != TimerStatus.RUNNING:
+            self.time_left = duration
             self.status = TimerStatus.RUNNING
             return True
         return False
@@ -31,11 +33,22 @@ class Timer:
 
     def stop(self):
         if self.status == TimerStatus.RUNNING:
+            self.time_left = 0
             self.status = TimerStatus.STOPPED
             return True
         return False
 
+    def tick(self, tick_duration=1):
+        self.time_left -= tick_duration
+        print('Time left: ', self.time_left)
+        if self.time_left <= 0:
+            self.status = TimerStatus.STOPPED
+            print('Timer stopped')
+
     def get_status(self):
         return self.status
+
+    def get_time(self):
+        return self.time_left
 
 
